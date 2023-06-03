a675925bf9479e6d9e5c3a51f39341ec1b6179be
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2023-06-03 10:32:11 -0500

    added icons to lualine

--- .config/nvim/lua/_lualine.lua
+++ .config/nvim/lua/_lualine.lua
@@ -19,10 +19,10 @@ lsp_status.register_progress()
 -- lualine
 require('lualine').setup {
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     theme = 'material',
-    section_separators = { left = '', right = '' },
-    component_separators = { left = '|', right = '|' },
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
