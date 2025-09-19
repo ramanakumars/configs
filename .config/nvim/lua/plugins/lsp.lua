3b2a43d0f43ea04ed7b2ccd10d3b3441aa9eaef3
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2025-09-19 16:42:55 -0700

    adding json and updating lspconfig

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -301,7 +301,8 @@ return {
 					-- by the server configuration above. Useful when disabling
 					-- certain features of an LSP (for example, turning off formatting for ts_ls)
 					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-					require("lspconfig")[server_name].setup(server)
+					vim.lsp.enable(server_name)
+					vim.lsp.config(server_name, server)
 				end,
 			},
 		})
